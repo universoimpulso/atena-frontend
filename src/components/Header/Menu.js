093859cc3f10c42068cb3860d059f9dc9ccc98a1
Julio Goncalves <julio-goncalves@outlook.com>
2019-06-13 09:11:06 -0300

    WiP

--- src/components/Header/Menu.js
+++ src/components/Header/Menu.js
@@ -1,40 +1,5 @@
 import React, { Fragment } from "react";
-import PropTypes from "prop-types";
-import LinkedInPage from "../LinkedIn";
 import StyledMenu from "./Menu.style";
-
-const links = user => {
-  const whithoutAuth = [
-    {
-      class: "login",
-      title: "login",
-      link: "/auth/linkedin"
-    }
-  ];
-  const withAuth = [
-    {
-      class: "logout",
-      title: "Sair",
-      link: "/auth/logout"
-    }
-  ];
-
-  const options = user ? withAuth : whithoutAuth;
-
-  return [
-    {
-      class: "index",
-      title: "como funciona",
-      link: "/"
-    },
-    {
-      class: "ranking",
-      title: "ranking",
-      link: "/ranking"
-    },
-    ...options
-  ];
-};
 
 const ProfileUser = ({ avatar }) => (
   <p className="profile">
@@ -42,26 +7,19 @@ const ProfileUser = ({ avatar }) => (
   </p>
 );
 
-ProfileUser.propTypes = {
-  avatar: PropTypes.string.isRequired
-};
-
 const renderLinks = ({ user }) => (
   <Fragment>
-    <li key={0}>
+    <li key="index">
       <a className="index" href={"/"}>
         como funciona
       </a>
     </li>
-    <li key={1}>
+    <li key="ranking">
       <a className="ranking" href={"/ranking"}>
         ranking
       </a>
     </li>
 
-    <li>
-      <LinkedInPage />
-    </li>
     {user && (
       <li className="user">
         <ProfileUser avatar={user.avatar} />
@@ -69,10 +27,6 @@ const renderLinks = ({ user }) => (
     )}
   </Fragment>
 );
-
-renderLinks.propTypes = {
-  user: PropTypes.object
-};
 
 const Menu = props => <StyledMenu>{renderLinks(props)}</StyledMenu>;
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4390"
+  baseURL: "https://staging-atena-network.herokuapp.com"
 });
 
 export default api;
